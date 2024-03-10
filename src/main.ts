574be3a2daaf3c72e2746e2d19934d3c11448074
Sakari Marttinen <sakari.marttinen@helsinki.fi>
2024-03-10 06:51:49 +0200

    Add cookies

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,12 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <h1>Hellou!</h1>
 
-    <p class="read-the-docs">
+    <p class="dont-read-the-docs">
       I am Kussi "Sakari" Marttinen and I don't like frontend. I did however create this veri great website for you to enjoy,
       as I got tired of seeing a 404 when I tried going to my website. It was created with Vite so I had to put veri low effort
       and it doesn't even have any vulnerabilities! (100% cybersecure!) </p>
 
-    <p class="read-the-docs">
+    <p class="dont-read-the-docs">
       If you are a jobperson please don't listen to me too closely and insted watch this cat gif I added (with my veri big skills)
     </p>
 
@@ -17,8 +17,16 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
       <img src=/cat.gif />
     </a>
 
-    <p class="read-the-docs">
+    <p class="dont-read-the-docs">
     Also please see my <a href=https://linkedin.com/in/kussi> linkedin </a> and <a href=https://github.com/hojott/> github</a>.
+    </p>
+  </div>
+  <div>
+    </p>
+    </p>
+    <p class="read-the-docs">
+      <a href=https://orteil.dashnet.org/cookieclicker>This</a> site uses cookies. By being on this site you accept that
+      (and even if you don't I can't do a lot about it)
     </p>
   </div>
 `
