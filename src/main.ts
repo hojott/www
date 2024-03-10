fadd288f3f49bef1f0f2ffd804f91cd96dd84cc6
Sakari Marttinen <sakari.marttinen@helsinki.fi>
2024-03-10 05:51:07 +0200

    Create Vanilla TS implementation

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import './style.css'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    <h1>Hellou!</h1>
+
+    <p class="read-the-docs">
+      I am Kussi "Sakari" Marttinen and I don't like frontend. I did however create this veri great website for you to enjoy,
+      as I got tired of seeing a 404 when I tried going to my website. It was created with Vite so I had to put veri low effort
+      and it doesn't even have any vulnerabilities! (100% cybersecure!) </p>
+
+    <p class="read-the-docs">
+      If you are a jobperson please don't listen to me too closely and insted watch this cat gif I added (with my veri big skills)
+    </p>
+
+    <a href=https://blog.dnevnik.hr/malecka22/2009/03/index >
+      <img src=/cat.gif />
+    </a>
+
+    <p class="read-the-docs">
+    Also please see my <a href=https://linkedin.com/in/kussi> linkedin </a> and <a href=github.com/hojott/> github</a>.
+    </p>
+  </div>
+`
